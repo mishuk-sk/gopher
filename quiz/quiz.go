f40658a7ee3d54ed9356086ea74cd34ff9ef410a
mishuk <mishuk.sk@live.ru>
2019-08-18 21:31:57 +0300

    done with time canceling

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
@@ -9,26 +10,65 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"sync/atomic"
+	"time"
 )
 
+type question struct {
+	Q string
+	A int
+}
+
 func main() {
-	fn := flag.String("f", "filename", "Quiz questions file")
-	flag.Parse()
-	f, err := os.Open(*fn)
-	defer f.Close()
-	if err != nil {
-		fmt.Printf("Can't open file with filename: %s\n", *fn)
-		log.Fatalf("Cant open file with name %s, with err: %s\n", *fn, err)
-	}
 	l, err := os.Create("logs.out")
 	if err != nil {
 		fmt.Println("Something wrong with logs. Contact me. Err: ", err)
 	}
 	defer l.Close()
 	log.SetOutput(l)
+	fn := flag.String("f", "filename", "Quiz questions file")
+	timeLimit := flag.Int("t", 30, "Time limit to quiz")
+	flag.Parse()
+	ctx, close := context.WithTimeout(context.Background(), time.Duration(*timeLimit)*time.Second)
+	defer close()
+	quiz := readQuiz(*fn)
+	var ta int64
+	go func() {
+		r := bufio.NewReader(os.Stdin)
+		for _, qst := range quiz {
+			fmt.Print(qst.Q, "=")
+
+			// TODO Qst: How to cancel reading from io to terminate goroutine correctly
+			text, err := r.ReadString('\n')
+			if err != nil {
+				fmt.Println("Program ran on internal err.")
+				log.Fatalf("Can't read from stdin. Err: %s", err)
+			}
+			text = text[:len(text)-1]
+			ans, err := strconv.Atoi(text)
+			if err != nil {
+				log.Printf("Can't parse user input to record (%s=%d). Answer is: %s. Err: %s\n", qst.Q, qst.A, text, err)
+				continue
+			}
+			if qst.A == ans {
+				atomic.AddInt64(&ta, 1)
+			}
+		}
+		close()
+	}()
+	<-ctx.Done()
+	fmt.Printf("You answered %d of %d questions\n", ta, len(quiz))
+}
+
+func readQuiz(filename string) []question {
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+		fmt.Printf("Can't open file with filename: %s\n", filename)
+		log.Fatalf("Cant open file with name %s, with err: %s\n", filename, err)
+	}
+	questions := make([]question, 0)
 	r := csv.NewReader(f)
-	tq := 0
-	ta := 0
 	for {
 		record, err := r.Read()
 		if err == io.EOF {
@@ -46,22 +86,7 @@ func main() {
 			log.Printf("Can't convert answer to integer. Err: %s\n", err)
 			continue
 		}
-		fmt.Print(qst, "=")
-		tq++
-		rStdin := bufio.NewReader(os.Stdin)
-		text, err := rStdin.ReadString('\n')
-		if err != nil {
-			fmt.Println("Program ran on internal err.")
-			log.Fatalf("Can't read from stdin. Err: %s", err)
-		}
-		usrAns, err := strconv.Atoi(text[:len(text)-1])
-		if err != nil {
-			log.Printf("Can't parse user input to record (%v). Answer is: %s. Err: %s\n", record, text, err)
-			continue
-		}
-		if usrAns == ans {
-			ta++
-		}
+		questions = append(questions, question{qst, ans})
 	}
-	fmt.Printf("You answered %d of %d questions\n", ta, tq)
+	return questions
 }
