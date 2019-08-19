6ef69016114886c5e25292fe061219b81a8a323e
mishuk <mishuk.sk@live.ru>
2019-08-19 13:01:48 +0300

    made it more readable and testable

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -2,91 +2,96 @@ package main
 
 import (
 	"bufio"
-	"context"
 	"encoding/csv"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"os"
-	"strconv"
-	"sync/atomic"
+	"strings"
 	"time"
 )
 
-type question struct {
+type problem struct {
 	Q string
-	A int
+	A string
 }
 
 func main() {
+	//#############################
+	//Initializing all io thing
 	l, err := os.Create("logs.out")
 	if err != nil {
 		fmt.Println("Something wrong with logs. Contact me. Err: ", err)
 	}
 	defer l.Close()
 	log.SetOutput(l)
-	fn := flag.String("f", "filename", "Quiz questions file")
-	timeLimit := flag.Int("t", 30, "Time limit to quiz")
+	fn := flag.String("f", "quiz.csv", "Quiz questions csv file in format 'question,answer'")
+	timeLimit := flag.Int("t", 30, "Time limit for the quiz")
 	flag.Parse()
-	ctx, close := context.WithTimeout(context.Background(), time.Duration(*timeLimit)*time.Second)
-	defer close()
-	quiz := readQuiz(*fn)
-	var ta int64
-	go func() {
-		r := bufio.NewReader(os.Stdin)
-		for _, qst := range quiz {
-			fmt.Print(qst.Q, "=")
-
-			// TODO Qst: How to cancel reading from io to terminate goroutine correctly
-			text, err := r.ReadString('\n')
-			if err != nil {
-				fmt.Println("Program ran on internal err.")
-				log.Fatalf("Can't read from stdin. Err: %s", err)
-			}
-			text = text[:len(text)-1]
-			ans, err := strconv.Atoi(text)
-			if err != nil {
-				log.Printf("Can't parse user input to record (%s=%d). Answer is: %s. Err: %s\n", qst.Q, qst.A, text, err)
-				continue
-			}
-			if qst.A == ans {
-				atomic.AddInt64(&ta, 1)
-			}
-		}
-		close()
-	}()
-	<-ctx.Done()
-	fmt.Printf("You answered %d of %d questions\n", ta, len(quiz))
-}
-
-func readQuiz(filename string) []question {
-	f, err := os.Open(filename)
+	f, err := os.Open(*fn)
 	defer f.Close()
 	if err != nil {
-		fmt.Printf("Can't open file with filename: %s\n", filename)
-		log.Fatalf("Cant open file with name %s, with err: %s\n", filename, err)
+		fmt.Printf("Can't open file with file name: %s\n", *fn)
+		log.Fatalf("Cant open file with name %s, with err: %s\n", *fn, err)
 	}
-	questions := make([]question, 0)
-	r := csv.NewReader(f)
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatalf("Error reading records: %s\n", err)
+	//############################
+	quiz := readQuiz(f)
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+	answers := handleQuiz(quiz, timer)
+	fmt.Printf("\nYou answered %d of %d questions\n", answers, len(quiz))
+}
+
+func handleQuiz(quiz []problem, timer *time.Timer) int {
+	var answers int
+	r := bufio.NewReader(os.Stdin)
+	ansChan := make(chan string, 1)
+	for _, qst := range quiz {
+		fmt.Print(qst.Q, "=")
+		go readInput(r, ansChan)
+		select {
+		case <-timer.C:
+			return answers
+		case ans := <-ansChan:
+			if ans == qst.A {
+				answers++
+			}
 		}
+	}
+	return answers
+}
+
+func readInput(r *bufio.Reader, answers chan<- string) {
+	go func() {
+		ans, err := r.ReadString('\n')
+		if err != nil {
+			fmt.Println("Program ran into an internal err.")
+			log.Fatalf("Can't read from stdin. Err: %s", err)
+		}
+		ans = strings.TrimSpace(ans[:len(ans)-1])
+		ans = strings.ToLower(ans)
+		answers <- ans
+	}()
+}
+
+func readQuiz(f io.Reader) []problem {
+	r := csv.NewReader(f)
+	records, err := r.ReadAll()
+	if err != nil {
+		log.Fatalf("Error reading records: %s\n", err)
+	}
+	problems := make([]problem, len(records))
+	for i, record := range records {
 		if len(record) != 2 {
 			log.Printf("Record (%v) is not valid length\n", record)
 			continue
 		}
-		qst := record[0]
-		ans, err := strconv.Atoi(record[1])
-		if err != nil {
-			log.Printf("Can't convert answer to integer. Err: %s\n", err)
-			continue
+		answer := strings.TrimSpace(record[1])
+		answer = strings.ToLower(answer)
+		problems[i] = problem{
+			Q: record[0],
+			A: answer,
 		}
-		questions = append(questions, question{qst, ans})
 	}
-	return questions
+	return problems
 }
