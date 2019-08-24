5a8cfeaea5807246d0f5b9953e6c2d0d70fb323b
mishuk <mishuk.sk@live.ru>
2019-08-24 15:08:13 +0300

    removed unnecessary test

--- linkparser/linkparser_test.go
+++ linkparser/linkparser_test.go
@@ -1,35 +1,12 @@
 package linkparser
 
 import (
-	"fmt"
-	"io/ioutil"
-	"os"
 	"strconv"
 	"strings"
 	"testing"
 
 	"golang.org/x/net/html"
 )
-
-func TestLinks(t *testing.T) {
-	info, err := ioutil.ReadDir("tests")
-	if err != nil {
-		t.Fatalf("Can't find directory 'tests'. Err - %s", err)
-	}
-	for _, f := range info {
-		if !f.IsDir() {
-			file, err := os.Open("tests/" + f.Name())
-			if err != nil {
-				t.Fatal(err)
-			}
-			links, err := Parse(file)
-			if err != nil {
-				t.Fatal(err)
-			}
-			fmt.Println(links)
-		}
-	}
-}
 
 type Test struct {
 	input    string
