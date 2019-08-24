e37636674a7da80d4446741a16171c53281cad7e
mishuk <mishuk.sk@live.ru>
2019-08-24 23:51:10 +0300

    fisrt, full of mistakes implementation

---
+++ sitemap/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/mishuk-sk/gopher/sitemap"
+
+func main() {
+	sitemap.Map("https://www.calhoun.io/")
+}
---
+++ sitemap/sitemap.go
@@ -0,0 +1,54 @@
+package sitemap
+
+import (
+	"fmt"
+	"net/http"
+	"runtime"
+	"sync"
+	"sync/atomic"
+
+	"github.com/mishuk-sk/gopher/linkparser"
+)
+
+// Map returns site map as xml for provided url
+func Map(url string) ([]byte, error) {
+	pages := make(map[string]bool)
+	cpu := runtime.NumCPU()
+	shutdown := make(chan struct{})
+	ch := make(chan string, cpu)
+	ch <- url
+	var wg sync.WaitGroup
+	wg.Add(cpu)
+	var counter int64
+	for i := 0; i < cpu; i++ {
+		go func() {
+			for atomic.LoadInt64(&counter) < 3 {
+				select {
+				case u := <-ch:
+					pages[u] = true
+					p, err := http.Get(u)
+					if err == nil {
+						links, _ := linkparser.Parse(p.Body)
+						go func() {
+							for _, l := range links {
+								if _, ok := pages[l.Href]; !ok {
+									ch <- l.Href
+								}
+							}
+						}()
+					}
+
+					atomic.AddInt64(&counter, 1)
+				case <-shutdown:
+					return
+				}
+			}
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	for k := range pages {
+		fmt.Println(k)
+	}
+	return nil, nil
+}
