dfb11f583255d29157aba380844f0264a16c4efe
mishuk <mishuk.sk@live.ru>
2019-08-31 17:47:59 +0300

    added test for sorting

--- deck/deck_test.go
+++ deck/deck_test.go
@@ -11,10 +11,24 @@ func TestNew(t *testing.T) {
 	if len(d) != 52 {
 		t.Fatalf("Expected deck to be of 52 cards but got %d\n Deck: %v\n", len(d), d)
 	}
-	for i := deck.Spades; i <= deck.Hearts; i++ {
+	for i := deck.Spade; i <= deck.Heart; i++ {
 		for j := deck.Ace; j <= deck.King; j++ {
-			if c := d[int(i-deck.Spades)*13+int(j-deck.Ace)]; c.Suit != i || c.Rank != j {
+			if c := d[int(i-deck.Spade)*13+int(j-deck.Ace)]; c.Suit != i || c.Rank != j {
 				t.Fatalf("Expected deck to be properly sorted\n Deck:%v\n", d)
+			}
+		}
+	}
+}
+
+func TestSort(t *testing.T) {
+	d := deck.New()
+	deck.Sort(d, func(i, j int) bool {
+		return d[i].Rank < d[j].Rank
+	})
+	for i := deck.Ace; i <= deck.King; i++ {
+		for j := 0; j < 4; j++ {
+			if index := int(i-deck.Ace)*4 + j; d[index].Rank != i {
+				t.Fatalf("Expected to get %s on position %d, but got %s\n", i, index, d[index].Rank)
 			}
 		}
 	}
