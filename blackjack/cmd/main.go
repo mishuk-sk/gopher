31d3416f677f387a63064da372d04876e27b8d48
mishuk <mishuk.sk@live.ru>
2019-09-01 18:11:14 +0300

    smth little working

--- blackjack/cmd/main.go
+++ blackjack/cmd/main.go
@@ -1,1 +1,28 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/mishuk-sk/gopher/blackjack"
+	"github.com/mishuk-sk/gopher/deck"
+)
+
+func main() {
+	r := rand.New(rand.NewSource(time.Now().Unix()))
+	player := blackjack.NewPlayer("Player1", func(str string, ctx context.Context) bool {
+
+		n := r.Intn(3)
+		return n == 1
+	})
+	player.OnChange(func(m map[string][]deck.Card) {
+		for k, v := range m {
+			fmt.Printf("%s: %s\n", k, v)
+		}
+	})
+	table := blackjack.NewTable(player)
+	table.Start()
+	<-time.After(time.Second * 3)
+}
