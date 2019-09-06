615f0ee0afa35726310d7adc854af87a8da12bd3
mishuk <mishuk.sk@live.ru>
2019-09-06 08:06:29 +0300

    removed unnecessary comments

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -58,60 +58,6 @@ func (p Player) Notify(data interface{}) {
 	fmt.Println(data)
 }
 
-/*
-type Game struct {
-	Deck    deck.Deck
-	Players []*Player
-}
-
-func NewGame(name string, deckOpts ...deck.Option) *Game {
-	return &Game{
-		Deck:    deck.New(deckOpts...),
-		Players: []*Player{},
-	}
-}
-
-func (t *Game) Start() (<-chan *Player, error) {
-	if len(t.Players) == 0 {
-		return nil, fmt.Errorf("Can't start blackjack game with 0 players")
-	}
-	t.Deck = deck.Shuffle(t.Deck)
-	dealer := NewPlayer(func(interface{}, context.Context) {}, func() bool {
-		r := rand.New(rand.NewSource(time.Now().Unix()))
-		return r.Intn(3) == 0
-	})
-	handleGame(t, dealer)
-	return nil, nil
-}
-
-func handleGame(g *Game, dealer *Player) error {
-	deck := &g.Deck
-	card, err := deck.Card()
-	if err != nil {
-		return err
-	}
-	GiveCard(dealer, card)
-	for i := 0; i < 2; i++ {
-		for _, p := range g.Players {
-			card, err := deck.Card()
-			if err != nil {
-				return err
-			}
-			GiveCard(p, card)
-		}
-	}
-	notifyPlayers("Some info", g.Players...)
-}
-
-func notifyPlayers(data interface{}, players ...*Player) {
-	ctx, _ := context.WithTimeout(context.Background(), time.Second*3)
-	for _, p := range players {
-		go func() {
-			p.Notification(data, ctx)
-		}()
-	}
-}
-*/
 const (
 	StatePlayerTurn = iota
 	StateDealerTurn
