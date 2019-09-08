8c99252dac285391fa5c69ec89c1b2289e548cc3
mishuk <mishuk.sk@live.ru>
2019-09-08 15:14:29 +0300

    added abstraction for dealer implementation

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -8,21 +8,20 @@ import (
 	"github.com/mishuk-sk/gopher/deck"
 )
 
+type Participant interface {
+	Hit() bool
+	Notify(interface{})
+}
+type Hand []deck.Card
+
+func NewHand() Hand {
+	return []deck.Card{}
+}
+
 type Player struct {
-	Cards []deck.Card
+	Hand
 }
 
-func NewPlayer() Player {
-	return Player{
-		Cards: []deck.Card{},
-	}
-}
-
-//GiveCard adds card to player's cards and returns current score
-func (p *Player) GiveCard(card deck.Card) int {
-	p.Cards = append(p.Cards, card)
-	return calcScore(p.Cards)
-}
 func (p Player) Hit() bool {
 	fmt.Println("Choose to hit or stay")
 	var in string
@@ -35,12 +34,16 @@ func (p Player) Hit() bool {
 	}
 	return false
 }
+func (p Player) Notify(data interface{}) {
+	fmt.Println(data)
+}
 func calcScore(cards []deck.Card) int {
 	aces := 0
 	score := 0
 	for _, c := range cards {
 		if c.Rank == deck.Ace {
 			aces++
+			score++
 		}
 		switch c.Rank {
 		case deck.Ace, deck.King, deck.Queen, deck.Jake, deck.Ten:
@@ -50,12 +53,34 @@ func calcScore(cards []deck.Card) int {
 		}
 	}
 	for ; score > 21 && aces > 0; aces-- {
-		score -= 9
+		score -= 10
 	}
 	return score
 }
-func (p Player) Notify(data interface{}) {
-	fmt.Println(data)
+
+type Dealer struct {
+	Hand
+}
+
+func (d Dealer) Hit() bool {
+	score := calcScore(d.Hand)
+	return score < 16 || (score == 17 && hasAce(d.Hand))
+}
+func (d Dealer) Notify(data interface{}) {}
+
+func hasAce(cards []deck.Card) bool {
+	for i := range cards {
+		if cards[i].Rank == deck.Ace {
+			return true
+		}
+	}
+	return false
+}
+
+//GiveCard adds card to player's cards and returns current score
+func GiveCard(h *Hand, card deck.Card) int {
+	*h = append(*h, card)
+	return calcScore(*h)
 }
 
 const (
@@ -68,32 +93,50 @@ type GameState struct {
 	Deck      deck.Deck
 	State     int
 	Players   []Player
-	Dealer    Player
+	Dealer    Dealer
 	curPlayer int
 }
 
-func (gs *GameState) CurrentPlayer() *Player {
+func (gs *GameState) CurrentPlayer() Participant {
 	switch gs.State {
 	case StatePlayerTurn:
-		return &gs.Players[gs.curPlayer]
+		return gs.Players[gs.curPlayer]
 	case StateDealerTurn:
-		return &gs.Dealer
+		return gs.Dealer
+	default:
+		panic("Not a turn")
+	}
+}
+func (gs *GameState) CurrentHand() *Hand {
+	switch gs.State {
+	case StatePlayerTurn:
+		return &gs.Players[gs.curPlayer].Hand
+	case StateDealerTurn:
+		return &gs.Dealer.Hand
 	default:
 		panic("Not a turn")
 	}
 }
 
 func Start() {
+	participants := []Player{
+		Player{
+			Hand: NewHand(),
+		},
+		Player{
+			Hand: NewHand(),
+		},
+	}
 	gs := GameState{
 		Deck:    deck.New(deck.AddDecks(2), deck.Shuffle),
-		Players: []Player{NewPlayer(), NewPlayer(), NewPlayer()},
-		Dealer:  NewPlayer(),
+		Players: participants,
+		Dealer:  Dealer{NewHand()},
 	}
 	gs = Deal(gs)
 	ctx, close := context.WithTimeout(context.Background(), time.Second*5)
 	defer close()
 	for gs.State == StatePlayerTurn {
-		for hitStay(ctx, *gs.CurrentPlayer()) {
+		for hitStay(ctx, gs.CurrentPlayer()) {
 			gs = Hit(gs)
 			close()
 			ctx, close = context.WithTimeout(context.Background(), time.Second*5)
@@ -101,29 +144,38 @@ func Start() {
 		gs = Stay(gs)
 	}
 	close()
+	ctx, close = context.WithTimeout(context.Background(), time.Second*5)
+	defer close()
 	for gs.State == StateDealerTurn {
-		gs.State = StateHandOver
+		for hitStay(ctx, gs.CurrentPlayer()) {
+			gs = Hit(gs)
+			close()
+			ctx, close = context.WithTimeout(context.Background(), time.Second*5)
+		}
+		gs = Stay(gs)
 	}
+	close()
 	fmt.Println(gs.Players)
 	fmt.Println(gs.Dealer)
 }
 
 func Deal(gs GameState) GameState {
-	dealQueue := make([]*Player, 0, len(gs.Players)+1)
-	dealQueue = append(dealQueue, &gs.Dealer)
+	dealQueue := make([]*Hand, 0, len(gs.Players)+1)
+	dealQueue = append(dealQueue, &(gs.Dealer.Hand))
 	for i := range gs.Players {
-		dealQueue = append(dealQueue, &gs.Players[i])
+		dealQueue = append(dealQueue, &gs.Players[i].Hand)
 	}
 	for i := 0; i < 2; i++ {
-		for _, p := range dealQueue {
+		for _, h := range dealQueue {
 			c, _ := gs.Deck.Card()
-			p.GiveCard(c)
+			GiveCard(h, c)
 		}
 	}
 	gs.State = StatePlayerTurn
 	return gs
 }
-func hitStay(ctx context.Context, p Player) bool {
+
+func hitStay(ctx context.Context, p Participant) bool {
 	choice := make(chan bool, 1)
 	go func() {
 		choice <- p.Hit()
@@ -138,12 +190,15 @@ func hitStay(ctx context.Context, p Player) bool {
 
 func Hit(gs GameState) GameState {
 	c, _ := gs.Deck.Card()
-	gs.CurrentPlayer().GiveCard(c)
+	h := gs.CurrentHand()
+	GiveCard(h, c)
 	return gs
 }
 
 func Stay(gs GameState) GameState {
-	if gs.curPlayer == len(gs.Players)-1 {
+	if gs.State == StateDealerTurn {
+		gs.State = StateHandOver
+	} else if gs.curPlayer == len(gs.Players)-1 {
 		gs.State = StateDealerTurn
 	} else {
 		gs.curPlayer++
